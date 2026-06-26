18804f8cb1e1dfffaea392f1f5ab152775d1ea46
Mong'are Samuel Oenga <mongaresamuel08@gmail.com>
2026-06-26 14:42:53 +0300

    Refactor run.py to use environment variables

--- run.py
+++ run.py
@@ -3,5 +3,14 @@ from app import create_app
 
 app = create_app("development")
 
+import os
+from app import create_app
+
+app = create_app(os.environ.get("FLASK_ENV", "production"))
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=(os.environ.get("FLASK_ENV") == "development")
+    )
